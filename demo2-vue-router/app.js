6ee79de4faa9c092b8ca75c7eccd56876c1bae08
Gjincai <1262868349@qq.com>
2017-02-23 18:52:01 +0800

    id:router

--- demo2-vue-router/app.js
+++ demo2-vue-router/app.js
@@ -1,9 +1,18 @@
 const Foo = { template: '<div>foo</div>' }
 const Bar = { template: '<div>bar</div>' }
+const User = {
+	template: '<div>User: {{ $route.params.id }}</div>'
+}
+const Find = {
+	template: '<div>Find: {{ $route.params }}</div>'
+}
 
 const routes = [
 	{ path: '/foo', component: Foo },
-	{ path: '/bar', component: Bar }
+	{ path: '/bar', component: Bar },
+	{ path: '/user', component: User },
+	{ path: '/user/:id', component: User},	//'/user/:id',冒号之后不能有空格
+	{ path: '/find/:class/post/:post_id', component: Find}	//'/user/:id',冒号之后不能有空格
 ]
 
 const router = new VueRouter({
