de6dec601f3208a9c21fa9c49937833b704f23fc
Jam <guo3jin3cai@gmail.com>
2017-02-27 23:56:26 +0800

    view

--- demo2-vue-router/app.js
+++ demo2-vue-router/app.js
@@ -6,13 +6,17 @@ const User = {
 const Find = {
 	template: '<div>Find: {{ $route.params }}</div>'
 }
+const Name = {
+	template: '<div>Name: {{ $route.params }}</div>'
+}
 
 const routes = [
 	{ path: '/foo', component: Foo },
 	{ path: '/bar', component: Bar },
 	{ path: '/user', component: User },
 	{ path: '/user/:id', component: User},	//'/user/:id',冒号之后不能有空格
-	{ path: '/find/:class/post/:post_id', component: Find}	//'/user/:id',冒号之后不能有空格
+	{ path: '/find/:class/post/:post_id', component: Find},	//'/user/:id',冒号之后不能有空格
+	{ path: '/name/nameId', name: 'namerouter', component: Name}
 ]
 
 const router = new VueRouter({
@@ -22,6 +26,38 @@ const router = new VueRouter({
 var app = new Vue({
 	router
 }).$mount('.routerVue');
+
+
+const Index = { template: '<div>Index</div>' }
+const Other1 = { template: '<div>Other1</div>' }
+const Other2 = { template: '<div>Other2</div>' }
+
+const router2 = new VueRouter({
+  // mode: 'history',
+  routes: [
+    { path: '/',
+      // a single route can define multiple named components
+      // which will be rendered into <router-view>s with corresponding names.
+      components: {
+        default: Index,
+        a: Other1,
+        b: Other2
+      }
+    },
+    {
+      path: '/other',
+      components: {
+        default: Other2,
+        a: Other1,
+        b: Index
+      }
+    }
+  ]
+})
+
+var app2 = new Vue({
+	router2
+}).$mount('.routerVue2');
 
 
 
